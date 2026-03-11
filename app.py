13d902c332ea83f632149da05de4ae4b5962c3de
Maxime RIO <maxime.rio@niwa.co.nz>
2026-03-11 21:48:26 +1300

    allow setting user and session

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 os.environ["MLFLOW_USE_DEFAULT_TRACER_PROVIDER"] = "false"
 
@@ -10,11 +11,11 @@ from strands.models.ollama import OllamaModel
 
 
 @mlflow.trace
-def process_message(user_input):
+def process_message(user_input, user_id, session_id):
     mlflow.update_current_trace(
         metadata={
-            "mlflow.trace.user": "test_user",
-            "mlflow.trace.session": "test_session",
+            "mlflow.trace.user": user_id,
+            "mlflow.trace.session": session_id,
         }
     )
     msg = st.session_state.agent(prompt)
@@ -24,16 +25,21 @@ def process_message(user_input):
 mlflow.set_experiment("Test Experiment")
 mlflow.strands.autolog()
 
-# TODO add widget to enter username in sidebar
-# TODO add button to start new session (use uuid for session name)
+with st.sidebar:
+    user_id = st.text_input("User ID", "test_user")
+    new_session = st.button("New session")
 
-if "agent" not in st.session_state:
+if "agent" not in st.session_state or new_session:
     model_id = os.environ["MODEL_ID"]
     ollama_host = os.environ.get("OLLAMA_HOST", "http://localhost:11434")
     ollama.Client(ollama_host).pull(model_id)
     model = OllamaModel(host=ollama_host, model_id=model_id)
     agent = Agent(model=model)
     st.session_state.agent = agent
+    st.session_state.session_id = str(uuid.uuid4())
+
+with st.sidebar:
+    st.caption(f"**Session ID:** {st.session_state.session_id}")
 
 for message in st.session_state.agent.messages:
     with st.chat_message(message["role"]):
@@ -43,5 +49,5 @@ for message in st.session_state.agent.messages:
 if prompt := st.chat_input("Say something"):
     with st.chat_message("user"):
         st.markdown(prompt)
-    process_message(prompt)
+    process_message(prompt, user_id, st.session_state.session_id)
     st.rerun()
